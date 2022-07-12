6fe198241fca5b4de16daf92b924ba67bc13bb9a
kasettakorn <kasettakorn_techno@hotmail.com>
2022-07-12 23:47:12 +0700

    fixes: UnrealSubprocess condition

--- file.py
+++ file.py
@@ -4,23 +4,27 @@ import bcrypt, pyautogui as gui
 
 salt = b'$2b$12$u0VZrJCZLV3fFwqywWOKVe'
 
-if(not os.path.exists('password.txt')):
-    f = open('password.txt', 'w')
+def getPassword():
+    if(not os.path.exists('password.txt')):
+        f = open('password.txt', 'w')
 
-    password = gui.password(title='Input password', mask="*")
+        password = gui.password(title='Input password', mask="*")
 
-    password = password.encode('utf-8')
-    hashedPassword = bcrypt.hashpw(password, salt)
+        # password = password.encode('utf-8')
+        # hashedPassword = bcrypt.hashpw(password, salt)
 
-    f.write(hashedPassword.decode())
-    
+        # f.write(hashedPassword.decode())
+        f.write(str(password))
+    f = open('password.txt', 'r')
+    password = f.read() #str.encode(f.readline())
 
-f = open('password.txt', 'r')
-password = str.encode(f.readline())
+    return password
+        
 
-check = "".encode('utf-8')
 
-if bcrypt.checkpw(check, password):
-    print("login success")
-else:
-    print("incorrect password")
+# check = "".encode('utf-8')
+
+# if bcrypt.checkpw(check, password):
+#     print("login success")
+# else:
+#     print("incorrect password")
--- nino_kuni.py
+++ nino_kuni.py
@@ -1,65 +1,43 @@
 import pyautogui as gui
-import os, time, sys, threading
-import bcrypt
+import os, time, threading
 
-salt = b'$2b$12$u0VZrJCZLV3fFwqywWOKVe'
-
-if(not os.path.exists('password.txt')):
-    f = open('password.txt', 'w')
-    
-
-    password = "".encode('utf-8')
-    hashedPassword = bcrypt.hashpw(password, salt)
-
-    f.write(hashedPassword.decode())
-    
-
-f = open('password.txt', 'r')
-password = str.encode(f.readline())
-
-check = "".encode('utf-8')
-
-if bcrypt.checkpw(check, password):
-    print("login success")
-else:
-    print("incorrect password")
+from file import getPassword
 
 class batchThread(threading.Thread):
     def run(self):
         os.system("start " + os.environ["PUBLIC"] + "\\Desktop\\\"Netmarble Launcher\".lnk")
-
         pass
 
-if(open('password.txt', 'r+')):
-    print('yes')
-else:
-    print('no')
 
-thread = batchThread()
-thread.daemon = True
-thread.start()
+if __name__ == "__main__":
+    password = getPassword()
 
-time.sleep(5)
-print(1)
-gui.moveTo(1579,929)
-time.sleep(5)
-gui.leftClick()
-time.sleep(13)
-gui.leftClick()
-gui.moveTo(951,517)
-gui.leftClick()
+    thread = batchThread()
+    thread.daemon = True
+    thread.start()
+
+    time.sleep(5)
+    gui.moveTo(1579,929)
+    gui.leftClick()
+    while(True):
+        if os.system("TASKLIST | FINDSTR /I \"UnrealCEFSubProcess.exe\"") == 0:
+            break
+    time.sleep(5)
+    gui.leftClick()
+    gui.moveTo(951,512)
+    gui.leftClick()
 
 
-gui.write("", interval=0.1)
-gui.moveTo(1055, 812)
-gui.leftClick()
-gui.moveTo(1055, 712)
-time.sleep(3)
-gui.leftClick()
+    gui.write(password, interval=0.1)
+    gui.moveTo(1055, 812)
+    gui.leftClick()
+    gui.moveTo(1055, 712)
+    time.sleep(1)
+    gui.leftClick()
 
-while(True):
-    if os.system("TASKLIST | FINDSTR /I \"CrossWorlds.exe\"") != 0:
-        print("EXIT")
-        exit()
-   
-        
+    while(True):
+        if os.system("TASKLIST | FINDSTR /I \"CrossWorlds.exe\"") != 0:
+            print("EXIT")
+            exit()
+    
+            
